efe36a8f2b2eef76f17920036c945d56943a8b40
Jonas Karg <jonas.karg@hotmail.com>
2020-06-22 23:40:28 +0200

    inital commit, basic room layout

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Room from './Room';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Room />
+      <Room />
+    </div>
+  );
+}
---
+++ src/components/Room.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Room.scss';
+
+export default function Room() {
+  return (
+    <div className='room'>
+      <div className='top' />
+      <div className='left' />
+      <div className='right' />
+      <div className='bottom' />
+      <div className='back' />
+    </div>
+  );
+}
